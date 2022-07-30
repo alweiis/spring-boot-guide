5df86227b6d568a26718ce04ff08bc58451f2ff6
alweiis <jstm1207@gmail.com>
2022-07-30 20:32:47 +0900

    DTO 객체를 활용한 POST API 구현

--- api/src/main/java/com/springboot/api/controller/PostController.java
+++ api/src/main/java/com/springboot/api/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.springboot.api.controller;
 
+import com.springboot.api.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -20,5 +21,10 @@ public class PostController {
             sb.append(map.getKey() + " : " + map.getValue() + "\n");
         });
         return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMemberDto(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
     }
 }
