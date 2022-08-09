f3cbcbcae7f137bebc3d6b441cc25a1bc65186be
alweiis <jstm1207@gmail.com>
2022-08-09 23:43:45 +0900

    서비스 객체의 테스트 - saveProduct() 메서드의 단위 테스트

--- test/src/test/java/com/springboot/test/service/impl/ProductServiceTest.java
+++ test/src/test/java/com/springboot/test/service/impl/ProductServiceTest.java
@@ -1,15 +1,19 @@
 package com.springboot.test.service.impl;
 
+import com.springboot.test.data.dto.ProductDto;
 import com.springboot.test.data.dto.ProductResponseDto;
 import com.springboot.test.data.entity.Product;
 import com.springboot.test.data.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.AdditionalAnswers;
 import org.mockito.Mockito;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 public class ProductServiceTest {
@@ -41,5 +45,21 @@ public class ProductServiceTest {
         assertEquals(productResponseDto.getStock(), givenProduct.getStock());
 
         verify(productRepository).findById(123L);
+    }
+
+    @Test
+    void saveProductTest() {
+        Mockito.when(productRepository.save(any(Product.class)))
+                .then(returnsFirstArg());
+
+        ProductResponseDto productResponseDto = productService.saveProduct(
+                new ProductDto("펜", 1000, 1234)
+        );
+
+        assertEquals(productResponseDto.getName(), "펜");
+        assertEquals(productResponseDto.getPrice(), 1000);
+        assertEquals(productResponseDto.getStock(), 1234);
+
+        verify(productRepository).save(any());
     }
 }
