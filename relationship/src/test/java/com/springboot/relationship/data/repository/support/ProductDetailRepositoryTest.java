596ca11efb8a8d9b8532548144a1bb5d81953888
alweiis <jstm1207@gmail.com>
2022-09-10 21:19:34 +0900

    일대일 단방향 매핑 테스트 - 상품정보 테이블 -> 상품 테이블

---
+++ relationship/src/test/java/com/springboot/relationship/data/repository/support/ProductDetailRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.springboot.relationship.data.repository.support;
+
+import com.springboot.relationship.data.entity.Product;
+import com.springboot.relationship.data.entity.ProductDetail;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class ProductDetailRepositoryTest {
+
+    @Autowired
+    ProductDetailRepository productDetailRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void saveAndReadTest1() {
+        Product product = new Product();
+        product.setName("스프링 부트 JPA");
+        product.setPrice(5000);
+        product.setStock(500);
+
+        productRepository.save(product);
+
+        ProductDetail productDetail = new ProductDetail();
+        productDetail.setProduct(product);
+        productDetail.setDescription("스프링 부트와 JPA를 함께 볼 수 있는 책");
+
+        productDetailRepository.save(productDetail);
+
+        // 생성한 데이터 조회
+        log.info("savedProduct : " + productDetailRepository.findById(productDetail.getId()).get().getProduct());
+
+        log.info("savedProductDetail : " + productDetailRepository.findById(productDetail.getId()).get());
+    }
+
+}
