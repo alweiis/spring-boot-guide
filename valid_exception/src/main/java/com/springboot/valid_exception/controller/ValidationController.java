dd9dbeab9785a0453c87e9896ddc7772378a668b
alweiis <jstm1207@gmail.com>
2022-09-11 14:16:21 +0900

    @Validated을 이용한 검증 로직 추가

--- valid_exception/src/main/java/com/springboot/valid_exception/controller/ValidationController.java
+++ valid_exception/src/main/java/com/springboot/valid_exception/controller/ValidationController.java
@@ -1,9 +1,13 @@
 package com.springboot.valid_exception.controller;
 
 import com.springboot.valid_exception.data.dto.ValidRequestDto;
+import com.springboot.valid_exception.data.dto.ValidatedRequestDto;
+import com.springboot.valid_exception.data.group.ValidationGroup1;
+import com.springboot.valid_exception.data.group.ValidationGroup2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +25,33 @@ public class ValidationController {
             @Valid @RequestBody ValidRequestDto validRequestDto) {
         log.info(validRequestDto.toString());
         return ResponseEntity.status(HttpStatus.OK).body(validRequestDto.toString());
+    }
+
+    @PostMapping("/validated")
+    public ResponseEntity<String> checkValidation(
+            @Validated @RequestBody ValidatedRequestDto validatedRequestDto) {
+        log.info(validatedRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validatedRequestDto.toString());
+    }
+
+    @PostMapping("/validated/group1")
+    public ResponseEntity<String> checkValidation1(
+            @Validated(ValidationGroup1.class) @RequestBody ValidatedRequestDto validatedRequestDto) {
+        log.info(validatedRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validatedRequestDto.toString());
+    }
+
+    @PostMapping("/validated/group2")
+    public ResponseEntity<String> checkValidation2(
+            @Validated(ValidationGroup2.class) @RequestBody ValidatedRequestDto validatedRequestDto) {
+        log.info(validatedRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validatedRequestDto.toString());
+    }
+
+    @PostMapping("/validated/all-group")
+    public ResponseEntity<String> checkValidation3(
+            @Validated({ValidationGroup1.class, ValidationGroup2.class}) @RequestBody ValidatedRequestDto validatedRequestDto) {
+        log.info(validatedRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validatedRequestDto.toString());
     }
 }
