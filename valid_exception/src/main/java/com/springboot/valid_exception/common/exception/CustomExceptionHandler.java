247d3b3f242b529fd0fdd3aef140fb8eb693d277
alweiis <jstm1207@gmail.com>
2022-09-11 23:51:09 +0900

    커스텀 예외를 처리하는 메서드 추가

--- valid_exception/src/main/java/com/springboot/valid_exception/common/exception/CustomExceptionHandler.java
+++ valid_exception/src/main/java/com/springboot/valid_exception/common/exception/CustomExceptionHandler.java
@@ -29,4 +29,19 @@ public class CustomExceptionHandler {
 
         return new ResponseEntity<>(map, responseHeaders, httpStatus);
     }
+
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity<Map<String, String>> handleException(CustomException e,
+                                                               HttpServletRequest request) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        log.error("Advice 내 handleException 호출, {}, {}", request.getRequestURI(), e.getMessage());
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("error type", e.getHttpStatusType());
+        map.put("code", Integer.toString(e.getHttpStatusCode()));
+        map.put("message", e.getMessage());
+
+        return new ResponseEntity<>(map, responseHeaders, e.getHttpStatus());
+    }
 }
