0a05340459fb1f04e08b4a909c353d43497f48c7
alweiis <jstm1207@gmail.com>
2022-09-11 11:22:06 +0900

    initial commit(section 10)

---
+++ valid_exception/src/main/java/com/springboot/valid_exception/data/entity/Product.java
@@ -0,0 +1,32 @@
+package com.springboot.valid_exception.data.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@Table(name = "product")
+public class Product extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+}
