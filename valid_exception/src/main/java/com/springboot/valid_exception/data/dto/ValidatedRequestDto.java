fd11f391edc66312e2cac57c5136437f0d66b61e
alweiis <jstm1207@gmail.com>
2022-09-11 14:00:08 +0900

    유효성 검증을 위한 DTO 클래스 생성
    @Validated 어노테이션을 이용한 검증

---
+++ valid_exception/src/main/java/com/springboot/valid_exception/data/dto/ValidatedRequestDto.java
@@ -0,0 +1,39 @@
+package com.springboot.valid_exception.data.dto;
+
+import com.springboot.valid_exception.data.group.ValidationGroup1;
+import com.springboot.valid_exception.data.group.ValidationGroup2;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidatedRequestDto {
+
+    @NotBlank
+    String name;
+
+    @Email
+    String email;
+
+    @Pattern(regexp = "01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$")
+    String phoneNumber;
+
+    @Min(value = 20, groups = ValidationGroup1.class)
+    @Max(value = 40, groups = ValidationGroup1.class)
+    int age;
+
+    @Size(min = 0, max = 40)
+    String description;
+
+    @Positive(groups = ValidationGroup2.class)
+    int count;
+
+    @AssertTrue
+    boolean booleanCheck;
+}
