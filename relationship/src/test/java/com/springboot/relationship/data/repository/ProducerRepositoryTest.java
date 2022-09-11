f5d37c1e636ca98c8ac526c171816bc02ff7faec
alweiis <jstm1207@gmail.com>
2022-09-11 10:28:04 +0900

    다대다 양방향 매핑 테스트 - 상품 테이블 <-> 생산업체 테이블

--- relationship/src/test/java/com/springboot/relationship/data/repository/ProducerRepositoryTest.java
+++ relationship/src/test/java/com/springboot/relationship/data/repository/ProducerRepositoryTest.java
@@ -39,6 +39,34 @@ class ProducerRepositoryTest {
         System.out.println(producerRepository.findById(1L).get().getProducts());
     }
 
+    @Test
+    @Transactional
+    void relationshipTest2() {
+        Product product1 = saveProduct("펜", 500, 1000);
+        Product product2 = saveProduct("공책", 100, 2000);
+        Product product3 = saveProduct("지우개", 150,  500);
+
+        Producer producer1 = saveProducer("sun");
+        Producer producer2 = saveProducer("moon");
+
+        producer1.addProduct(product1);
+        producer1.addProduct(product2);
+        producer2.addProduct(product2);
+        producer2.addProduct(product3);
+
+        product1.addProducer(producer1);
+        product2.addProducer(producer1);
+        product2.addProducer(producer2);
+        product3.addProducer(producer2);
+
+        producerRepository.saveAll(Lists.newArrayList(producer1, producer2));
+        productRepository.saveAll(Lists.newArrayList(product1, product2, product3));
+
+        System.out.println("products : " + producerRepository.findById(1L).get().getProducts());
+
+        System.out.println("producers : " + productRepository.findById(2L).get().getProducers());
+    }
+
 
     private Product saveProduct(String name, Integer price, Integer stock) {
         Product product = new Product();
