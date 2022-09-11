3b69acff2113b78dfbddcd0efdd785df1d9a79ad
alweiis <jstm1207@gmail.com>
2022-09-11 10:37:37 +0900

    영속성 전이 테스트

--- relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
+++ relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
@@ -3,6 +3,7 @@ package com.springboot.relationship.data.repository;
 import com.springboot.relationship.data.entity.Product;
 import com.springboot.relationship.data.entity.Provider;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -78,5 +79,38 @@ class ProviderRepositoryTest {
         for (Product product : products) {
             System.out.println(product);
         }
+    }
+
+    @Test
+    void cascadeTest() {
+        Provider provider = savedProvider("새로운 공급업체");
+
+        Product product1 = savedProduct("상품1", 1000, 1000);
+        Product product2 = savedProduct("상품2", 500, 1500);
+        Product product3 = savedProduct("상품3", 750, 500);
+
+        // 연관관계 설정
+        product1.setProvider(provider);
+        product2.setProvider(provider);
+        product3.setProvider(provider);
+
+        provider.getProductList().addAll(Lists.newArrayList(product1, product2, product3));
+
+        providerRepository.save(provider);
+    }
+
+    private Provider savedProvider(String name) {
+        Provider provider = new Provider();
+        provider.setName(name);
+        return provider;
+    }
+
+    private Product savedProduct(String name, Integer price, Integer stock) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setStock(stock);
+
+        return product;
     }
 }
