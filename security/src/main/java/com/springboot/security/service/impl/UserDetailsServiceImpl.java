d1bb93de4714b1ffa6aaf0bf9b53d2330acabb56
alweiis <jstm1207@gmail.com>
2022-09-18 15:06:22 +0900

    UserDetails와 UserDetailsService 구현

---
+++ security/src/main/java/com/springboot/security/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.springboot.security.service.impl;
+
+import com.springboot.security.data.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        log.info("[loadUserByUsername] loadUserByUsername 수행. username : {}", username);
+        return userRepository.getByUid(username);
+    }
+}
