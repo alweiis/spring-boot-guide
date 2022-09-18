7d3b41ed1059ab186c75b39fe5f72e81119c2c48
alweiis <jstm1207@gmail.com>
2022-09-18 15:14:52 +0900

    회원가입과 로그인 로직 구현

---
+++ security/src/main/java/com/springboot/security/data/dto/SignUpResultDto.java
@@ -0,0 +1,19 @@
+package com.springboot.security.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpResultDto {
+
+    private boolean success;
+
+    private int code;
+
+    private String msg;
+
+}
